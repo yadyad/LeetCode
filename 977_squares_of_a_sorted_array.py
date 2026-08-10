9acc6bea99486317e512d72ac02af8c9c98120ec
yadyad <yadhup.krishna@gmail.com>
2026-08-10 19:38:43 +0530

    977 squares of a sorted array

---
+++ 977_squares_of_a_sorted_array.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        left = 0
+        right = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if abs(nums[left]) > abs(nums[right]):
+                res[i] = nums[left] ** 2
+                left += 1
+            else:
+                res[i] = nums[right] ** 2
+                right -= 1
+
+        return res
+
+
+s=Solution()
+print(s.sortedSquares([-4,-1,0,3,10]))
